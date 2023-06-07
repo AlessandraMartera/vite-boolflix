f545bbc189d27f0ba228de273a9ef5b31e8f3a34
alessandra martera <ale.martera@gmail.com>
2023-06-07 15:20:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from 'axios'
+import { store } from './store.js'
+import AppHeader from './components/AppHeader.vue'
+
+
+export default {
+  components: {
+    AppHeader
+  },
+
+  // star data
+  data() {
+    return {
+      store
+    }
+  },
+
+  // start methods
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style lang="scss"></style>
