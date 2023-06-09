189a005dc1b33d2fe2faadb19b6b273581c37125
alessandra martera <ale.martera@gmail.com>
2023-06-09 12:37:13 +0200

    update style

--- src/components/CardItems.vue
+++ src/components/CardItems.vue
@@ -19,50 +19,113 @@ export default {
 </script>
 
 <template>
-    <li>
-        <div>
+    <li class="card">
+
+        <div class="immage_card">
+
+
             <img :src="`${store.thumbImg}${details.backdrop_path}`" :alt="`motion picture ${details.title}${details.name}`">
         </div>
 
-        <div v-if="(store.movieList.includes(details))">
-            <!-- title -->
-            <div>
-                {{ details.title }}
+        <!-- v-if="(store.movieList.includes(details))" -->
+        <div class="text_card">
+            <div v-if="(store.movieList.includes(details))">
+                <!-- title -->
+                <div>
+                    <strong>Titolo: </strong> {{ details.title }}
+                </div>
+
+                <!-- original title -->
+                <div>
+                    <strong>Titolo Originale: </strong>{{ details.original_title }}
+                </div>
             </div>
 
-            <!-- original title -->
+            <div v-else-if="(store.tvList.includes(details))">
+                <!-- name -->
+                <div>
+                    <strong>Nome: </strong>{{ details.name }}
+                </div>
+
+                <!-- original name -->
+                <div>
+                    <strong>Nome Originale: </strong>{{ details.original_name }}
+                </div>
+            </div>
+
+            <!-- language -->
+
+            <span>
+                <strong>Lingua: </strong>
+            </span>
+            <div class="language">
+                <img :src="`language/${details.original_language}.jpg`" :alt="`${details.original_language} language`">
+            </div>
+
+            <!-- vote -->
+            <div class="ratings">
+                <strong>Voto: </strong>
+                <ratingsStars v-for="(n, idx) in 5" :vote="details.vote_average" :pos="idx" />
+            </div>
+
             <div>
-                {{ details.original_title }}
+                <strong>overview: </strong>
+                {{ details.overview }}
             </div>
         </div>
 
-        <div v-else-if="(store.tvList.includes(details))">
-            <!-- name -->
-            <div>
-                {{ details.name }}
-            </div>
-
-            <!-- original name -->
-            <div>
-                {{ details.original_name }}
-            </div>
-        </div>
-
-        <!-- language -->
-        <div class="language">
-            <img :src="`language/${details.original_language}.jpg`" :alt="`${details.original_language} language`">
-
-        </div>
-
-        <!-- vote -->
-        <div class="ratings">
-            <ratingsStars v-for="(n, idx) in 5" :vote="details.vote_average" :pos="idx" />
-        </div>
 
     </li>
 </template>
 
 <style lang="scss" scoped>
+.card {
+    width: calc((100% /4) - 10px);
+    height: 400px;
+    background-color: #000;
+
+    .immage_card {
+
+        .title {
+            color: #ffd500;
+        }
+
+        display: block;
+        width: 100%;
+        height: 100%;
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+    .text_card {
+        display: none;
+    }
+}
+
+.card:hover {
+    .immage_card {
+        display: none;
+    }
+
+    .text_card {
+        display: block;
+        width: 100%;
+        height: 100%;
+        padding: 30px;
+        color: #fff;
+        border: 1px solid #fff;
+
+
+        div {
+
+            margin-bottom: 10px;
+        }
+    }
+}
+
 .language {
     width: 50px;
     height: 30px;
